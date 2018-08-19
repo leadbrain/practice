f63cbaca967b3c876bfc369136fc275444d5271b
yskim <ys.kim@riiid.co>
2018-08-20 08:57:39 +0900

    add test one spare

--- scoring_bowling/ruby/bowling.rb
+++ scoring_bowling/ruby/bowling.rb
@@ -8,6 +8,13 @@ class Bowling
   end
 
   def score
-    @down_pins.sum
+    @down_pins
+      .each_slice(2)
+      .map do |pins|
+        score = pins.first + pins.last
+        score += pins.first if score == 10
+        score
+      end
+      .sum
   end
 end
--- scoring_bowling/ruby/spec/bowling_spec.rb
+++ scoring_bowling/ruby/spec/bowling_spec.rb
@@ -16,4 +16,15 @@ describe Bowling do
       expect(bowling.score).to eq(20)
     end
   end
+
+  context 'one spare' do
+    it do
+      bowling = Bowling.new
+      bowling.roll(4)
+      bowling.roll(6)  # spare
+      bowling.roll(3)
+      17.times{ bowling.roll(0) }
+      expect(bowling.score).to eq(17)
+    end
+  end
 end
