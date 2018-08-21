ff90fa2f9f38c17635226956887bddf7db6a20a5
yskim <ys.kim@riiid.co>
2018-08-22 00:41:17 +0900

    make sub functions to remove ugly code

--- scoring_bowling/ruby/bowling2.rb
+++ scoring_bowling/ruby/bowling2.rb
@@ -11,24 +11,9 @@ class Bowling2
     @frame += 1
     @score += pin
 
-    #add bonus score
-    if @is_spare
-      @score += pin
-      @is_spare = false
-    end
-
-    if @strike_bonus_counter > 0
-      @score += pin
-      @strike_bonus_counter -= 1
-    end
-
-    #set status for next
-    if pin == 10
-      @frame += 1
-      @strike_bonus_counter += 2
-    elsif (((@frame % 2) == 0) && ((@prev_pin + pin) == 10))
-      @is_spare = true
-    end
+    add_bonus_score(pin)
+    
+    check_status(pin)
 
     @prev_pin = pin
   end
@@ -43,5 +28,26 @@ class Bowling2
 
   def end?
     (@frame / 2) == 10
+  end
+
+  def add_bonus_score(pin)
+    if @is_spare
+      @score += pin
+      @is_spare = false
+    end
+
+    if @strike_bonus_counter > 0
+      @score += pin
+      @strike_bonus_counter -= 1
+    end
+  end
+
+  def check_status(pin)
+    if pin == 10
+      @frame += 1
+      @strike_bonus_counter += 2
+    elsif (((@frame % 2) == 0) && ((@prev_pin + pin) == 10))
+      @is_spare = true
+    end
   end
 end
